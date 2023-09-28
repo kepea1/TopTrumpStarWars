da51070e722d753e4e0fe830be7f4743738cd4b5
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2023-09-28 18:43:20 +0100

    bug fixes, refactoring and updating css

--- my-app/src/components/app/index.js
+++ my-app/src/components/app/index.js
@@ -4,119 +4,121 @@ import ComputerCard from "../computerCard";
 import HumanCard from "../humanCard";
 import { useState, useEffect } from "react";
 
-export default function TrumpsApp({TrumpsAppVisible}) {
+export default function TrumpsApp({ TrumpsAppVisible }) {
+  const [id, setId] = useState(2);
+  const [userId, setUserId] = useState(1);
+  const [compcardvisible, compCardSetVisible] = useState(false);
 
-    const [id, setId] = useState(2);
-    const [userId, setUserId] = useState(1);
-    const [compcardvisible, compCardSetVisible] = useState(false);
-    // const [usercardvisible, userCardSetVisible] = useState(false);//    user card state
+  function newCard() {
+    let randomNumberComputer = Math.floor(Math.random() * 30 + 1);
+    let randomNumberUser = Math.floor(Math.random() * 30 + 1);
+    setId(randomNumberComputer);
+    setUserId(randomNumberUser);
+    console.log(setId);
+    console.log(setUserId);
+    compCardSetVisible(false); //hide computer card
+    setData("");
+  }
 
+  const [planet, setPlanet] = useState("Planet Pending...");
 
-
-    function handleClick() {
-        let randomNumberComputer = Math.floor(Math.random() * 30 + 1);
-        let randomNumberUser = Math.floor(Math.random() * 30 + 1);
-        setId(randomNumberComputer);
-        setUserId(randomNumberUser);
-        console.log(setId);
-        console.log(setUserId);
-        compCardSetVisible(false);//hide computer card
-        setData("");
+  useEffect(() => {
+    async function fetchCard() {
+      const response = await fetch(`https://swapi.dev/api/planets/${userId}/`);
+      const data = await response.json();
+      console.log(data);
+      setPlanet(data);
     }
-    
-       const [planet, setPlanet] = useState("Planet Pending...");
-       
-       useEffect(() => {
-           async function fetchCard() {
-               const response = await fetch(
-                   `https://swapi.dev/api/planets/${userId}/`
-               );
-               const data = await response.json();
-               console.log(data);
-               setPlanet(data);
-           }
-           fetchCard();
-       }, [userId]);
-      
-    // function userCard() {
-    //     userCardSetVisible(true);
-    // }
+    fetchCard();
+  }, [userId]);
 
-    const [compPlanet, setCompPlanet] = useState("Planet Pending...");
-    //console.log(id);
-    useEffect(() => {
-        async function fetchCard() {
-            const response = await fetch(`https://swapi.dev/api/planets/${id}`);
-            const data = await response.json();
-            console.log(data);
-            setCompPlanet(data);
-        }
-        fetchCard();
-    }, [id]);
+  // function userCard() {
+  //     userCardSetVisible(true);
+  // }
 
-
-
-    function challengeComputer() {
-        compCardSetVisible(true);
-        comparescores();
+  const [compPlanet, setCompPlanet] = useState("Planet Pending...");
+  //console.log(id);
+  useEffect(() => {
+    async function fetchCard() {
+      const response = await fetch(`https://swapi.dev/api/planets/${id}`);
+      const data = await response.json();
+      console.log(data);
+      setCompPlanet(data);
     }
+    fetchCard();
+  }, [id]);
 
-   
-// alert coming too soon maybe put it in a use effect fuction?
+  function challengeComputer(childdata) {
+    //chack if a check box is clicked and alert if not
+    compCardSetVisible(true);
+    comparescores();
+  }
 
-    function comparescores(userChoise) {
-        console.log(userChoise)
-        const userStat = userChoise[0];
-        const userValue = userChoise[1];
-        const computerValue = compPlanet[userStat]
-        console.log(computerValue)
+  // alert coming too soon maybe put it in a use effect fuction?
 
-        if (userValue > computerValue) {
-            alert("you won");
-            console.log("youwon")
-        } else if (userValue < computerValue) {
-            alert("you lost");
-        } else {
-            alert("Draw");
-        }
-//                        comparescores(["rotation_period",planet.rotation_period])
+  function comparescores(userChoise) {
+    if (userChoise === undefined) {
+      alert("please select a stat");
+    } else {
+      console.log(userChoise);
+      const userStat = userChoise[0];
+      const userValue = userChoise[1];
+      const computerValue = compPlanet[userStat];
+      console.log(computerValue);
 
-
+      if (userValue > computerValue) {
+        alert("you won");
+        console.log("youwon");
+      } else if (userValue < computerValue) {
+        alert("you lost");
+      } else {
+        alert("Draw");
+      }
+      //                        comparescores(["rotation_period",planet.rotation_period])
     }
+  }
 
-    //function to take data from children to check computer score
+  //function to take data from children to check computer score
 
-    const [data, setData] = useState("");
-    const childToParent = (childdata) => {
-        setData(childdata);
-    };
+  const [data, setData] = useState("");
 
-    if (TrumpsAppVisible === true) {
-        return (
+  const childToParent = (childdata) => {
+    setData(childdata);
+  };
 
-        <div>
-        
-                <h1>Star Wars Top Trumps</h1>
-            <view className="cardLayout">
-
-                {/* <HumanCard  className="HCard" id={userId} childToParent={childToParent} visible={usercardvisible}/> */}
-                <HumanCard  className="HCard" id={userId} childToParent={childToParent} comparescores={comparescores} planet={planet} />
-                <ComputerCard className="CCard" id={id} visible={compcardvisible} compPlanet={compPlanet} />
-
-            </view>
-            <view className="cardTitles">
-                <h2>User Card:</h2>
-                <h2> Computer Card:</h2>
-            </view>
-                <p>Youve Chosen:{data}</p>
-
-                <button className="newcard" onClick={handleClick}>
-                    Generate New Card
-                </button>
-                <button className="challengecomputer" onClick={challengeComputer}>
-                    Challenge Computer
-                </button>
+  if (TrumpsAppVisible === true) {
+    return (
+      <div>
+        <h1>Star Wars Top Trumps</h1>
+        <div className="cardLayout">
+          {/* <HumanCard  className="HCard" id={userId} childToParent={childToParent} visible={usercardvisible}/> */}
+          <HumanCard
+            className="HCard"
+            id={userId}
+            childToParent={childToParent}
+            comparescores={comparescores}
+            planet={planet}
+          />
+          <ComputerCard
+            className="CCard"
+            id={id}
+            visible={compcardvisible}
+            compPlanet={compPlanet}
+          />
         </div>
+        <div className="cardTitles">
+          <h2>your Card:</h2>
+          <h2> Computer Card:</h2>
+        </div>
+        <p>Youve Chosen:{data}</p>
+
+        <button className="newcard" onClick={newCard}>
+          Generate New Card
+        </button>
+        <button className="challengecomputer" onClick={challengeComputer}>
+          Challenge Computer
+        </button>
+      </div>
     );
-        }
+  }
 }
--- my-app/src/components/computerCard/index.js
+++ my-app/src/components/computerCard/index.js
@@ -1,61 +1,47 @@
 import "./index.css";
 
-//import useeffect and useState from react
-import { useEffect, useState } from "react";
-
 //repass id as props when randmoly generating and set as dependency
 
 function ComputerCard({ id, visible, setComputerCard, compPlanet }) {
-    //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
-    // const [planet, setPlanet] = useState("Planet Pending...");
-    // //console.log(id);
-    // useEffect(() => {
-    //     async function fetchCard() {
-    //         const response = await fetch(`https://swapi.dev/api/planets/${id}`);
-    //         const data = await response.json();
-    //         console.log(data);
-    //         setPlanet(data);
-    //     }
-    //     fetchCard();
-    // }, [id]);
-    
-    if (visible === true) {
-        return (
-            <div className="computer-card">
-                <h1 className="title">Planet Name: {compPlanet.name}</h1>
+  if (visible === true) {
+    return (
+      <div className="computer-card">
+        <h1 className="title">Planet Name: {compPlanet.name}</h1>
 
-                <div className="statistic">
-                    <p className="stat_name">Rotation Period: </p>
-                    <p className="stat_value">{compPlanet.rotation_period}</p>
-                    <br type="checkbox"></br>
-                </div>
+        <div className="statistic">
+          <p className="stat_name">Rotation Period: </p>
+          <p className="stat_value">{compPlanet.rotation_period}</p>
+          <br type="checkbox"></br>
+        </div>
 
-                <div className="statistic">
-                    <p className="stat_name">Orbital Period:</p>
-                    <p className="stat_value">{compPlanet.orbital_period}</p>
-                    <br type="checkbox"></br>
-                </div>
+        <div className="statistic">
+          <p className="stat_name">Orbital Period:</p>
+          <p className="stat_value">{compPlanet.orbital_period}</p>
+          <br type="checkbox"></br>
+        </div>
 
-                <div className="statistic">
-                    <p className="stat_name">Diameter: </p>
-                    <p className="stat_value">{compPlanet.diameter}</p>
-                    <br type="checkbox"></br>
-                </div>
+        <div className="statistic">
+          <p className="stat_name">Diameter: </p>
+          <p className="stat_value">{compPlanet.diameter}</p>
+          <br type="checkbox"></br>
+        </div>
 
-                <div className="statistic">
-                    <p className="stat_name">Population: </p>
-                    <p className="stat_value">{compPlanet.population}</p>
-                    <br type="checkbox"></br>
-                </div>
+        <div className="statistic">
+          <p className="stat_name">Population: </p>
+          <p className="stat_value">{compPlanet.population}</p>
+          <br type="checkbox"></br>
+        </div>
 
-                <div className="statistic">
-                    <p className="stat_name">Surface Water:</p>
-                    <p className="stat_value">{compPlanet.surface_water}</p>
-                    <br type="checkbox"></br>
-                </div>
-            </div>
-        );
-    }
+        <div className="statistic">
+          <p className="stat_name">Surface Water:</p>
+          <p className="stat_value">{compPlanet.surface_water}</p>
+          <br type="checkbox"></br>
+        </div>
+      </div>
+    );
+  } else {
+    return <div className="computer-card"></div>;
+  }
 }
 
 export default ComputerCard;
